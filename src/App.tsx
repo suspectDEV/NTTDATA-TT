43110c027a06af870c84acab318c288a84cfebc6
suspect.dev <alexander.forerolopez@gmail.com>
2022-11-15 04:45:54 +0100

    new styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import FormComponent from './components/form';
+import './styles/styles.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<FormComponent />} />
+
       </Routes>
     </BrowserRouter>
   );
